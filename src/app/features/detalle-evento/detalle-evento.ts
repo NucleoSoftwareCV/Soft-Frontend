cbf09956b4018a13fb3cad8cb78238f348d18256
Mijael <mijaelalexisds@gmail.com>
2026-07-02 10:31:59 -0500

    Agregué modal de filtros y mejoras en detalle de eventos

--- src/app/features/detalle-evento/detalle-evento.ts
+++ src/app/features/detalle-evento/detalle-evento.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ExperienciasService } from '../../services/experiencias';
+import { map, switchMap, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-detalle-evento',
@@ -11,28 +12,67 @@ import { ExperienciasService } from '../../services/experiencias';
   styleUrl: './detalle-evento.css'
 })
 export class DetalleEvento {
-  evento: any;
-  similares: any[] = [];
+  evento$!: Observable<any>;
+  similares$!: Observable<any[]>;
+
+  imagenesPlaceholder = [
+    'https://images.unsplash.com/photo-1506126613408-eca07ce68773?w=1200',
+    'https://images.unsplash.com/photo-1545389336-cf090694435e?w=800',
+    'https://images.unsplash.com/photo-1515377905703-c4788e51af15?w=800',
+    'https://images.unsplash.com/photo-1545389336-cf090694435e?w=800',
+    'https://images.unsplash.com/photo-1515377905703-c4788e51af15?w=800',
+  ];
 
   constructor(
     private route: ActivatedRoute,
     private experienciasService: ExperienciasService
   ) {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.evento$ = this.route.paramMap.pipe(
+      switchMap(params => {
+        const id = Number(params.get('id'));
 
-    this.evento = this.experienciasService.getExperienciaById(id);
+        return this.experienciasService.getExperienciaById(id).pipe(
+          map(evento => ({
+            ...evento,
+            imagenes: this.imagenesPlaceholder,
+            favorito: false
+          }))
+        );
+      })
+    );
 
-    this.similares = this.experienciasService
-      .getExperiencias()
-      .filter(item => item.id !== id)
-      .slice(0, 4);
+    this.similares$ = this.route.paramMap.pipe(
+      switchMap(params => {
+        const id = Number(params.get('id'));
+
+        return this.experienciasService.getExperiencias().pipe(
+          map(resp =>
+            resp.content
+              .filter((item: any) => item.id !== id)
+              .slice(0, 4)
+          )
+        );
+      })
+    );
   }
 
-  getGalleryCount() {
-    return Math.min(this.evento?.imagenes?.length || 0, 5);
+  getGalleryCount(evento: any) {
+    return Math.min(evento?.imagenes?.length || 0, 5);
   }
 
-  toggleFavorito() {
-    this.evento.favorito = !this.evento.favorito;
+  getImagenesGaleria(evento: any) {
+    return evento?.imagenes?.slice(0, 5) || [];
+  }
+
+  getImagenesExtra(evento: any) {
+    return (evento?.imagenes?.length || 0) - 5;
+  }
+
+  hayImagenesExtra(evento: any) {
+    return (evento?.imagenes?.length || 0) > 5;
+  }
+
+  toggleFavorito(evento: any) {
+    evento.favorito = !evento.favorito;
   }
 }
--- src/app/features/explorar/explorar.ts
+++ src/app/features/explorar/explorar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { ExperienciasService } from '../../services/experiencias';
@@ -10,15 +10,54 @@ import { ExperienciasService } from '../../services/experiencias';
   templateUrl: './explorar.html',
   styleUrl: './explorar.css'
 })
-export class Explorar {
+export class Explorar implements OnInit {
   experiencias: any[] = [];
 
-  constructor(private experienciasService: ExperienciasService) {
-    this.experiencias = this.experienciasService.getExperiencias();
+  paginaActual = 0;
+  tamanioPagina = 12;
+  totalExperiencias = 0;
+  hayMas = true;
+  cargando = false;
+
+  imagenPlaceholder = 'https://images.unsplash.com/photo-1506126613408-eca07ce68773?w=800';
+
+  mostrarFiltros = false;
+
+  constructor(private experienciasService: ExperienciasService) {}
+
+  ngOnInit() {
+    this.cargarMas();
+  }
+
+  cargarMas() {
+    if (this.cargando || !this.hayMas) return;
+
+    this.cargando = true;
+
+    this.experienciasService.getExperiencias(this.paginaActual, this.tamanioPagina).subscribe({
+      next: (resp) => {
+        const nuevosEventos = resp.content.map((item: any) => ({
+          ...item,
+          favorito: false,
+          imagen: this.imagenPlaceholder
+        }));
+
+        this.experiencias = [...this.experiencias, ...nuevosEventos];
+        this.totalExperiencias = resp.totalElements;
+        this.hayMas = !resp.last;
+        this.paginaActual++;
+        this.cargando = false;
+      },
+      error: (err) => {
+        console.error('Error al cargar eventos:', err);
+        this.cargando = false;
+      }
+    });
   }
 
   toggleFavorito(item: any) {
     item.favorito = !item.favorito;
   }
+
   
 }
