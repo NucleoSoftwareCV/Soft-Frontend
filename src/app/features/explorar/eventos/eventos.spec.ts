03ae0d8684f90a9e345f289c2da3a6a08fb3fdca
Said Lopez <said.lopez.dev@gmail.com>
2026-08-09 17:52:56 -0500

    fix: align home layout and prerender behavior

--- src/app/features/explorar/eventos/eventos.spec.ts
+++ src/app/features/explorar/eventos/eventos.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideRouter } from '@angular/router';
 
 import { Eventos } from './eventos';
 
@@ -14,6 +15,7 @@ describe('Explorar', () => {
       providers: [
         provideHttpClient(),
         provideHttpClientTesting(),
+        provideRouter([]),
       ],
     }).compileComponents();
 
